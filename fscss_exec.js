d8e4c30b5591ea4d5c0c355583157cb27ed3b2f1
Figsh <safetyunion5550@gmail.com>
2026-03-14 22:23:34 +0100

    Update fscss_exec.js

--- fscss_exec.js
+++ fscss_exec.js
@@ -1,4 +1,4 @@
-import { exec } from "https://cdn.jsdelivr.net/npm/fscss@1.1.11/e/xfscss.min.js";
+import { exec } from "https://cdn.jsdelivr.net/npm/fscss@1.1.17/e/xfscss.min.js";
 async function applyStyle(){
   new exec({
     type: "", 
